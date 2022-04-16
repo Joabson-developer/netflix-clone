014038be571ddab9c85f959f436724eb6e9c5f28
Joabson-developer <83815302+Joabson-developer@users.noreply.github.com>
2022-04-16 00:14:15 -0300

    Construção do footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 // Modules
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserModule } from '@angular/platform-browser';
 
 // Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/pages/home/home.component';
 import { MovieRowComponent } from './shared/movie-row/movie-row.component';
 import { FeaturedMovieComponent } from './shared/featured-movie/featured-movie.component';
+import { HeaderMovieComponent } from './shared/header-movie/header-movie.component';
+import { FooterAppComponent } from './shared/footer-app/footer-app.component';
 
 // Directives
-import { BackgroundImageDirective } from './directive/background-image.directive';
+import { BackgroundImageDirective } from './directive/background-image/background-image.directive';
+import { ColorDirective } from './directive/color/color.directive';
 
-// Angular Material
-import { MatIconModule } from '@angular/material/icon';
+// Pipes
 import { GetFullYearPipe } from './pipes/date/get-full-year.pipe';
 import { MaxLengthPipe } from './pipes/string/max-length.pipe';
-import { HeaderMovieComponent } from './shared/header-movie/header-movie.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +33,8 @@ import { HeaderMovieComponent } from './shared/header-movie/header-movie.compone
     GetFullYearPipe,
     MaxLengthPipe,
     HeaderMovieComponent,
+    FooterAppComponent,
+    ColorDirective,
   ],
   imports: [
     BrowserModule,
